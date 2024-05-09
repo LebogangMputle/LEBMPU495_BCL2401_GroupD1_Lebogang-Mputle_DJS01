8cbc6bad250daa7b4a6e0f186295d23bb6a00983
LebogangMputle <lebozikalala@gmail.com>
2024-05-09 10:35:45 +0200

    Converted the initial velocity from km/h to m/s before the calculation.

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
-// Check if the units of the given parameters are consistent
-const isNumber = (variable) => Object.prototype.toString.call(variable) === '[object Number]';
-
-const checkParameters = () => [vel, acc, time, d, fuel, fbr].every(isNumber);
-
-const checkFunction = (func, numArgs) => typeof func === 'function' && func.length === numArgs;
-
-if (checkParameters()) {
-  // Check if the time is not less than zero
-  if (time < 0) {
-    throw new Error('The time cannot be less than zero');
-  }
-}
-
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fuel - (fbr*time) //calculates remaining fuel
@@ -34,8 +20,8 @@ const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on accel
 
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-};
+  return (vel + (acc*time)) * 1000/3600;
+}
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
