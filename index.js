2b0c4e86c4c34eb297496629afa681817236a13c
LebogangMputle <lebozikalala@gmail.com>
2024-05-09 09:49:02 +0200

    Defined the function calcNewVel

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const rf = fuel - (fbr*time) //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
+const calcNewVel = (vel, acc, time) => { 
   return vel + (acc*time)
-}
+};
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
